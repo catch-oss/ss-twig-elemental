ad7605b9d7a16f1868f2292e6ed3868f80783336
Sam Etheridge <s@0k.nz>
2019-01-28 09:43:30 +1300

    initial commit

---
+++ src/Model/TwigElementalArea.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CatchDesign\SS\TwigElemental;
+
+use Azt3k\SS\Twig\TwigRenderer;
+use DNADesign\Elemental\Models\ElementalArea;
+
+class TwigElementalArea extends ElementalArea
+{
+     use TwigRenderer;
+}
