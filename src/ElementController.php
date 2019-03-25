0b53edaf02f00cefed04de2b2f749aedf85cbc54
Richard Davies <richard@catchdesign.co.nz>
2019-03-25 17:39:55 +1300

    [NZLS003-36] Control the Twig elemental code in a tighter way

---
+++ src/ElementController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CatchDesign\SS\TwigElemental;
+
+use Azt3k\SS\Twig\TwigRenderer;
+use DNADesign\Elemental\Controllers\ElementController;
+
+class TwigElementController extends ElementController
+{
+     use TwigRenderer;
+
+     
+}
